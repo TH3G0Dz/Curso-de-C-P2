7717b6fc8f1e954293a2478a4db466dcf88b8922
Vitor Hugo <ferreiraindio2003@gmail.com>
2022-05-27 14:45:24 -0300

    Add files via upload

---
+++ CURSO-DE-C/forca.h
@@ -0,0 +1,10 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+int acertou();
+int enforcou();
+int jachutou(char letra);
